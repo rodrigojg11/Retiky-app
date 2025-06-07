641325c2a2ced8406507916d48f2dab1a8d56ddc
Evelyn Aviles <eellkkmm@gmail.com>
2025-06-07 13:42:46 -0600

    filtro

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,13 @@
 import { Application } from "@hotwired/stimulus"
 import OfferController from "./search_controller"
 
+import ValidationController from "./validation_controller"
+application.register("validation", ValidationController)
+
+import { application } from "./application"
+import FlightFormController from "./flight_form_controller"
+application.register("flight-form", FlightFormController)
+
+
 const application = Application.start()
 application.register("search", SearchController)
