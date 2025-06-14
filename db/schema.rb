b66430d12fb0e251464a34dba7fe78c7a8086768
joaquin taiba <joaquin.taiba@uc.cl>
2025-06-14 16:54:44 -0400

    filtros precios y descuento

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,8 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema[7.1].define(version: 2025_06_11_003523) do
+
+ActiveRecord::Schema[7.1].define(version: 2025_06_14_202127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +60,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_11_003523) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "lightning_start_time"
+    t.float "final_price"
   end
 
   create_table "users", force: :cascade do |t|
