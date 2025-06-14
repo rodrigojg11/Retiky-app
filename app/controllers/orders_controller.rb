f884f96e6c5f5f0cf5c0efe7cbdf2e51b2a1ba70
Rodrigo Jaramillo <rodrigojaramilloga@gmail.com>
2025-06-14 14:01:02 -0500

    Last changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.create(user_id: current_user.id, ticket_id: params[:ticket_id])
-    redirect_to orders_path
+    redirect_to orders_path(@order), notice: "Your order was placed successfully!"
   end
 
   def index
