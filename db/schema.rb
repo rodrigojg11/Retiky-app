6067280e3a3c6381e89d4c2ee516fdbc7c985b03
ana delano <analaura_dlano@outlook.com>
2025-06-14 10:50:32 -0600

    cambios en diseño de tickets

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema[7.1].define(version: 2025_06_11_003523) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +40,17 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_11_003523) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "offers", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "origin"
+    t.string "destination"
+    t.date "departure_date"
   end
 
   create_table "orders", force: :cascade do |t|
